d5b4c531d6300febd58ee4063fed36575dd06bf2
Matixo55 <47039608+Matixo55@users.noreply.github.com>
2020-03-09 16:55:54 +0100

    version 2

--- script.py
+++ script.py
@@ -4,57 +4,59 @@ import http.server
 import requests
 import socketserver
 
+
+class Website:
+    def __init__(self, link):
+        self.link = link
+        self.text = config[link]["Required"]
+        self.max_time = int(config[link]["MaxTime"])
+        self.error = None
+        try:
+            html = requests.get(self.link, timeout=self.max_time)
+            self.status = html.status_code
+            self.latency = round(requests.get(self.link).elapsed.total_seconds(), 3)
+        except ConnectionError:
+            self.error = "Couldn't connect"
+        except TimeoutError as error:
+            self.error = str(error)
+        except BaseException as exception:
+            self.error = str(exception)
+        if self.error is None:
+            if self.text not in html:
+                self.error = "Required text not found on website"
+
+
 config = configparser.ConfigParser()
-# Prepare files
 config.read('config.ini')
+websites = []
 log = open('log.txt', "a")
-log_website = open("Index.html", "w+")
-# Check current time
-log.write("\n\n'{}\n".format(datetime.datetime.now()))
+log_website = open("Index.html", "w")
+log.write("{}\n".format(datetime.datetime.now()))
 log_website.write("<html>\n<body>\n<p><b>{}</b>".format(datetime.datetime.now()))
-# Illiterate over website links
-print("Starting process")
-for link in config.sections():
-    # Load configuration
-    website = config[link]
-    required = website["Required"]
-    try:
-        log.write("Website: {}\nRequired text: {}".format(link, required))
-        log_website.write("\n<br><br>Website: <a href='{}'>{}</a>".format(link, link))
-        # Connect to website
-        html = requests.get(link, timeout=int(website["MaxTime"]))
-        # Check status code
-        log.write("\nStatus code: {}".format(html.status_code))
-        log_website.write("\n<br>Status code: {}".format(html.status_code))
-    # Handle errors
-    except ConnectionError:
-        log.write("\nERROR Couldn't connect\n\n")
-        log_website.write("\n<br><font color='red'>ERROR Couldn't connect</font>")
-        continue
-    except TimeoutError as error:
-        log.write("\nERROR {}\n\n".format(error))
-        log_website.write("\n<br><font color='red'>ERROR {}</font>".format(error))
-        continue
-    except BaseException as exception:
-        log.write("\nERROR {}\n\n".format(exception))
-        log_website.write("\n<br><font color='red'>ERROR {}</font>".format(exception))
-        continue
-    if required not in html.text:
-        log.write("\nERROR Required text not found on website\n\n")
-        log_website.write("\n<br><font color='red'>ERROR Required text not found on website</font>")
-        continue
-    # Check response time
-    time = requests.get(link).elapsed.total_seconds()
-    log.write("\nResponse time: {}s".format(round(time, 3)))
-    log_website.write("\n<br>Response time: {}s".format(round(time, 3)))
-# Finish process
+
+print("Reading config")
+for url in config.sections():
+    websites.append(Website(url))
+
+print("Checking websites")
+for url in websites:
+    log.write("Website: {}\nRequired text: {}".format(url.link, url.text))
+    log_website.write("\n<br><br>Website: <a href='{}'>{}</a>".format(url.link, url.link))
+    log.write("\nStatus code: {}".format(url.status))
+    log_website.write("\n<br>Status code: {}".format(url.status))
+    if url.error is None:
+        log.write("\nERROR {}\n\n".format(url.error))
+        log_website.write("\n<br><font color='red'>ERROR {}</font>".format(url.error))
+    else:
+        log.write("\nResponse time: {}s\n\n".format(url.latency))
+        log_website.write("\n<br>Response time: {}s".format(url.latency))
+
 log_website.write("</p>\n</html>\n</body>")
 log.close()
 log_website.close()
-print("Latencies checked")
 port = int(config["DEFAULT"]["ServerPort"])
 handler = http.server.SimpleHTTPRequestHandler
-# Start local server
+
 print("Creating local server")
 with socketserver.TCPServer(("", port), handler) as httpd:
     httpd.serve_forever()
