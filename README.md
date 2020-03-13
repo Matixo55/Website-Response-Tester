99e31c6fb76a0d612101ad9d8df28060b5be95ea
Matixo55 <47039608+Matixo55@users.noreply.github.com>
2020-03-13 22:26:58 +0100

    13.03.2020

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-#Website response time checker
-A simple script alowing you to control website's status and response time.
+# Website response time
+A simple script allowing you to control website's status and response time.
 Including log, local server hosting results and simple configuration options.
--- script.py
+++ script.py
@@ -1,8 +1,79 @@
 import configparser
 import datetime
 import http.server
+import time
+import re
 import requests
 import socketserver
+
+
+def create_website():
+    print("Creating local website")
+    log_website.write("<html>\n<body>\n<p><b>{}</b>".format(datetime.datetime.now()))
+    for url in websites:
+        log_website.write("\n<br><br>Website: <a href='{}'>{}</a>".format(url.link, url.link))
+        if url.error is not None:
+            log_website.write("\n<br><font color='red'>ERROR {}</font>".format(url.error))
+        else:
+            log_website.write("\n<br>Status code: {}".format(url.status))
+            log_website.write("\n<br><font color='green'>Response time: {}s</font>".format(url.latency))
+    log_website.write("</p>\n</html>\n</body>")
+    log_website.close()
+
+    port = int(config["DEFAULT"]["ServerPort"])
+    handler = http.server.SimpleHTTPRequestHandler
+    print("Creating local server\nType localhost:{} in Your browser".format(port))
+    server = socketserver.TCPServer(("", port), handler)
+    server.serve_forever(5)
+    server.shutdown()
+
+
+def http_requests():
+    print("Checking websites")
+    for url in config.sections():
+        websites.append(Website(url))
+
+
+def is_valid_url(url_to_check):
+    regex = re.compile(
+        r'^https?://'  # http:// or https://
+        r'(?:(?:[A-Z0-9](?:[A-Z0-9-]{0,61}[A-Z0-9])?\.)+[A-Z]{2,6}\.?|'  # domain...
+        r'localhost|'  # localhost...
+        r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})'  # ...or ip
+        r'(?::\d+)?'  # optional port
+        r'(?:/?|[/?]\S+)$', re.IGNORECASE)
+    if regex.search(url_to_check) is None:
+        raise ValueError
+
+
+def run():
+    setup_process()
+    http_requests()
+    update_log()
+    create_website()
+
+
+def setup_process():
+    print("Setting up")
+    global websites, log, log_website, config
+    config = configparser.ConfigParser()
+    config.read('config.ini')
+    log = open('log.txt', "a")
+    log_website = open("Index.html", "w")
+    websites = []
+
+
+def update_log():
+    print("Creating log")
+    log.write("{}\n".format(datetime.datetime.now()))
+    for url in websites:
+        log.write("Website: {}\nRequired text: {}".format(url.link, url.text))
+        if url.error is not None:
+            log.write("\nERROR {}\n\n".format(url.error))
+        else:
+            log.write("\nStatus code: {}".format(url.status))
+            log.write("\nResponse time: {}s\n\n".format(url.latency))
+    log.close()
 
 
 class Website:
@@ -12,51 +83,22 @@ class Website:
         self.max_time = int(config[link]["MaxTime"])
         self.error = None
         try:
+            is_valid_url(link)
             html = requests.get(self.link, timeout=self.max_time)
             self.status = html.status_code
             self.latency = round(requests.get(self.link).elapsed.total_seconds(), 3)
+            if self.text not in html.text:
+                self.error = "Required text not found on website"
+        except ValueError:
+            self.error = "Incorrect link"
         except ConnectionError:
             self.error = "Couldn't connect"
         except TimeoutError as error:
             self.error = str(error)
         except BaseException as exception:
             self.error = str(exception)
-        if self.error is None:
-            if self.text not in html:
-                self.error = "Required text not found on website"
 
 
-config = configparser.ConfigParser()
-config.read('config.ini')
-websites = []
-log = open('log.txt', "a")
-log_website = open("Index.html", "w")
-log.write("{}\n".format(datetime.datetime.now()))
-log_website.write("<html>\n<body>\n<p><b>{}</b>".format(datetime.datetime.now()))
-
-print("Reading config")
-for url in config.sections():
-    websites.append(Website(url))
-
-print("Checking websites")
-for url in websites:
-    log.write("Website: {}\nRequired text: {}".format(url.link, url.text))
-    log_website.write("\n<br><br>Website: <a href='{}'>{}</a>".format(url.link, url.link))
-    log.write("\nStatus code: {}".format(url.status))
-    log_website.write("\n<br>Status code: {}".format(url.status))
-    if url.error is None:
-        log.write("\nERROR {}\n\n".format(url.error))
-        log_website.write("\n<br><font color='red'>ERROR {}</font>".format(url.error))
-    else:
-        log.write("\nResponse time: {}s\n\n".format(url.latency))
-        log_website.write("\n<br>Response time: {}s".format(url.latency))
-
-log_website.write("</p>\n</html>\n</body>")
-log.close()
-log_website.close()
-port = int(config["DEFAULT"]["ServerPort"])
-handler = http.server.SimpleHTTPRequestHandler
-
-print("Creating local server")
-with socketserver.TCPServer(("", port), handler) as httpd:
-    httpd.serve_forever()
+while True:
+    run()
+    time.sleep(int(config["DEFAULT"]["Delay"]))
